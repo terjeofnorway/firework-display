6205935c9976c2bd8fd8bc9dd007253f13a5525d
terjeofnorway <terjeofnorway@gmail.com>
2016-09-17 11:31:52 +0200

    Ammended some Unit Tests for Tickable class

--- test/spec/tickable.spec.js
+++ test/spec/tickable.spec.js
@@ -19,6 +19,20 @@
       expect(eventSpy).toHaveBeenCalled();
     });
 
+    it('should attach an object referance as part of the ADD_TICKABLE_OJECT event', function(){
+      let tickTarget,
+        tickable;
+
+      document.addEventListener('ADD_TICKABLE_OBJECT', (e) => {
+        tickTarget = e.detail;
+      });
+
+      tickable = new Tickable();
+
+      expect(tickTarget instanceof Tickable).toBe(true);
+    });
+
+
 
     it('should fire and REMOVE_TICKABLE_OBJECT event at die()', () => {
       document.addEventListener('REMOVE_TICKABLE_OBJECT', eventSpy);
@@ -26,6 +40,22 @@
       tickable.die();
 
       expect(eventSpy).toHaveBeenCalled();
+    });
+
+
+
+    it('should attach an object referance as part of the REMOVE_TICKABLE_OJECT event', function(){
+      let tickTarget,
+        tickable;
+
+      document.addEventListener('REMOVE_TICKABLE_OBJECT', (e) => {
+        tickTarget = e.detail;
+      });
+
+      tickable = new Tickable();
+      tickable.die();
+
+      expect(tickTarget instanceof Tickable).toBe(true);
     });
 
 
